e6bd28f168e561634f80c899bf4b462337fd57e1
thelethalcode <kousshikraj.raj@gmail.com>
2019-03-27 23:39:04 +0530

    Added background image to the main screen

--- src/Library/src/Management_System.java
+++ src/Library/src/Management_System.java
@@ -1,27 +1,24 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.GroupLayout;
+import javax.imageio.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JLabel;
-import java.awt.Color;
-import java.awt.Font;
-import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
+
+import javax.swing.JTextArea;
 public class Management_System extends JFrame {
-	static Management_System frame;
-	private JPanel contentPane;
+
+    static Management_System frame;
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+        
+        // Puts it into the event queue so that it can be invoked later
+        EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+                    // Initialise the main class
 					frame= new Management_System();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -29,65 +26,93 @@ public class Management_System extends JFrame {
 				}
 			}
 		});
-	}
+	} 
 
-	/**
-	 * Create the frame.
-	 */
-	public Management_System() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		JLabel sportsManagement = new JLabel("Sports Management System");
-		sportsManagement.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		sportsManagement.setForeground(Color.GRAY);
-		
-		JButton btnGymkhanaLogin = new JButton("Gymkhana Login");
+    Management_System()
+    {
+        // Set the bounds
+        setBounds(100,100,500,500);
+        JPanel bg;
+
+        // Set Background Image
+        try {
+            Image backgroundImage = javax.imageio.ImageIO.read(new File("images/bg5.jpg"));
+            final Image bgima = backgroundImage.getScaledInstance(500, 500, Image.SCALE_DEFAULT);
+            bg = new JPanel(new BorderLayout()) {
+                @Override
+                public void paintComponent(Graphics g) {
+                    g.drawImage(bgima, 0, 80, null);
+                }
+            };
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Add the panel to the frame
+        setContentPane(bg);
+
+        // Set Heading
+        JTextArea heading = new JTextArea("\n   Gymkhana Sports Management System");
+        heading.setFont(new Font("DejaVu Sans", Font.BOLD, 20));
+        heading.setForeground(new Color(172,11,243));
+        heading.setBackground(new Color(52,0,100));
+        heading.setEditable(false);
+
+        // Login Button for Gymkhana Authorities
+        JButton btnGymkhanaLogin = new JButton("Admin Login");
 		btnGymkhanaLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			GymkhanaLogin.main(new String[]{});
 			frame.dispose();
 			}
 		});
-		btnGymkhanaLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		
+		btnGymkhanaLogin.setFont(new Font("Tahoma", Font.BOLD, 14));
+        
+        // Login Button for Users
 		JButton btnUserLogin = new JButton("User Login");
 		btnUserLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				UserLogin.main(new String[]{});
 				frame.dispose();
 			}
-		});
-		btnUserLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
+        });
+        btnUserLogin.setFont(new Font("Tahoma", Font.BOLD, 14));
+
+         
+        GroupLayout gl_contentPane = new GroupLayout(bg);
+        
+        // Set the horizontal layout
+        gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(64)
-							.addComponent(sportsManagement))
+							.addComponent(heading, GroupLayout.PREFERRED_SIZE, 500, GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(140)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-								.addComponent(btnUserLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(btnGymkhanaLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE))))
-					.addContainerGap(95, Short.MAX_VALUE))
-		);
+							.addGap(13)
+                            .addComponent(btnGymkhanaLogin, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+                            .addGap(210)
+                            .addComponent(btnUserLogin, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+                        )
+                    )
+                    .addContainerGap(95, Short.MAX_VALUE)
+                )
+        );
+        
+        // Set the vertical layout
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(sportsManagement)
-					.addGap(32)
-					.addComponent(btnGymkhanaLogin, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnUserLogin, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(70, Short.MAX_VALUE))
-		);
-		contentPane.setLayout(gl_contentPane);
-	}
-}
+                    .addComponent(heading, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+                    .addGap(330)
+                    .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+					.addComponent(btnGymkhanaLogin, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnUserLogin, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    )
+                    .addContainerGap(40, Short.MAX_VALUE)
+                )
+        );
+		bg.setLayout(gl_contentPane);
+
+    }
+};
