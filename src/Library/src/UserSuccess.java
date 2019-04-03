7e27e8cc263882feb8d0c0626fe7a2196a2ab1e0
thelethalcode <kousshikraj.raj@gmail.com>
2019-04-04 01:50:13 +0530

    Modified User Success

--- src/Library/src/UserSuccess.java
+++ src/Library/src/UserSuccess.java
@@ -1,18 +1,11 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JLabel;
-import java.awt.Font;
-import java.awt.Color;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.imageio.*;
+import java.io.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JComboBox;
 
 public class UserSuccess extends JFrame {
 	static UserSuccess frame;
@@ -28,7 +21,7 @@ public class UserSuccess extends JFrame {
 					frame = new UserSuccess();
 					frame.setVisible(true);
 				} catch (Exception e) {
-					e.printStackTrace();
+					System.out.println(e);
 				}
 			}
 		});
@@ -38,62 +31,58 @@ public class UserSuccess extends JFrame {
 	 * Create the frame.
 	 */
 	public UserSuccess() {
+
+		// Sets the default close operation
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 433);
-		contentPane = new JPanel();
-		contentPane.setForeground(Color.GRAY);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+
+		// Sets the bounds for the GUI
+		setBounds(100, 100, 500, 500);
 		
-		JLabel lblUserSection = new JLabel("Choose your activity:");
-		lblUserSection.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		JPanel bg;
 		
-		String[] sports = { "Badminton" , "Gym" , "Squash" , "Table Tennis" , "Swimming" };
+		// Set Background Image
+        try {
+            Image backgroundImage = javax.imageio.ImageIO.read(new File("images/userSuccess.jpg"));
+            final Image bgima = backgroundImage.getScaledInstance(635, 500, Image.SCALE_DEFAULT);
+            bg = new JPanel(new BorderLayout()) {
+                @Override
+                public void paintComponent(Graphics g) {
+                    g.drawImage(bgima, 0, 0, null);
+                }
+            };
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Add the panel to the frame
+        setContentPane(bg);
+		
+		// Heading Label
+		JLabel heading = new JLabel("Check Slot Availability");
+		heading.setForeground(new Color(200,250,230));
+		heading.setFont(new Font("Tahoma", Font.BOLD, 28));
+		
+		// The ComboBox of all possible sports
+		String[] sports = { "--- Choose Activity ---", "Badminton" , "Gym" , "Squash" , "Table Tennis" , "Swimming", "Tennis", "BasketBall" };
 		JComboBox list = new JComboBox(sports);
+		list.setBackground(new Color(43,54,54));
+		list.setForeground(new Color(210,180,180));
 		
-		JButton btnNewButton = new JButton("Check Slots");
-		btnNewButton.addActionListener(new ActionListener() {
+		// Check Slots button
+		JButton slotCheck = new JButton("Check Slots");
+		slotCheck.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			//SlotDao.main(new String[]{});
-			frame.dispose();
+				if(list.getSelectedIndex() != 0){
+
+				}
+				frame.dispose();
 			}
 		});
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		slotCheck.setFont(new Font("Tahoma", Font.BOLD, 15));
+		slotCheck.setBackground(new Color(17, 12, 18));
+		slotCheck.setForeground(Color.WHITE);
 		
-		// JButton btnViewBooks = new JButton("View Books");
-		// btnViewBooks.addActionListener(new ActionListener() {
-		// 	public void actionPerformed(ActionEvent arg0) {
-		// 		ViewBooks.main(new String[]{});
-		// 	}
-		// });
-		// btnViewBooks.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
-		// JButton btnIssueBook = new JButton("Issue Book");
-		// btnIssueBook.addActionListener(new ActionListener() {
-		// 	public void actionPerformed(ActionEvent e) {
-		// 		IssueBookForm.main(new String[]{});
-		// 		frame.dispose();
-		// 	}
-		// });
-		// btnIssueBook.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
-		// JButton btnViewIssuedBooks = new JButton("View Issued Books");
-		// btnViewIssuedBooks.addActionListener(new ActionListener() {
-		// 	public void actionPerformed(ActionEvent e) {
-		// 		ViewIssuedBooks.main(new String[]{});
-		// 	}
-		// });
-		// btnViewIssuedBooks.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
-		// JButton btnReturnBook = new JButton("Return Book");
-		// btnReturnBook.addActionListener(new ActionListener() {
-		// 	public void actionPerformed(ActionEvent e) {
-		// 		ReturnBook.main(new String[]{});
-		// 		frame.dispose();
-		// 	}
-		// });
-		// btnReturnBook.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+		// Logout Button
 		JButton btnLogout = new JButton("Logout");
 		btnLogout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -101,47 +90,50 @@ public class UserSuccess extends JFrame {
 				frame.dispose();
 			}
 		});
-		btnLogout.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		btnLogout.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnLogout.setBackground(new Color(17, 12, 18));
+		btnLogout.setForeground(Color.WHITE);
+
+		// Horizontal and Vertical layout for the panel
+		GroupLayout gl_contentPane = new GroupLayout(bg);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(132)
+					.addGap(70)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblUserSection, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-						.addComponent(list, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-						// .addComponent(btnReturnBook, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-						// .addComponent(btnViewIssuedBooks, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-						// .addComponent(btnIssueBook, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-						// .addComponent(btnViewBooks, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(101, Short.MAX_VALUE))
+						.addComponent(heading)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(85)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(list, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(26)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+										.addComponent(slotCheck)
+									)
+								)
+							)
+						)
+					)
+					.addContainerGap(101, Short.MAX_VALUE)
+				)
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
+					.addGap(28)
+					.addComponent(heading)
 					.addGap(58)
-					.addComponent(lblUserSection,GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
 					.addComponent(list,GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-					.addGap(68)
-					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+					.addGap(188)
+					.addComponent(slotCheck, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
-					// .addComponent(btnViewBooks, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-					// .addGap(18)
-					// .addComponent(btnIssueBook, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-					// .addGap(18)
-					// .addComponent(btnViewIssuedBooks, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-					// .addGap(18)
-					// .addComponent(btnReturnBook, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-					// .addGap(18)
 					.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(16, Short.MAX_VALUE))
+				)
 		);
-		contentPane.setLayout(gl_contentPane);
+		bg.setLayout(gl_contentPane);
 	}
 
 }
